bbe110bf9e888b02249cbdd3fe246111a28f1c36
nakibuddin <nakibaust137@gmail.com>
2022-11-11 06:16:31 +0600

    delete review api added

--- index.js
+++ index.js
@@ -61,6 +61,16 @@ async function run () {
             res.send(reviews);
         })
 
+    // delete reviews via review id (delete)
+        app.delete('/reviews/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = {_id : ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        
+        })
+
 
     }
     finally{
